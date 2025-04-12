d2da524a3da72079ff1c4fc5adf7b652efe5defd
Ranjitr123 <ranjit.r@terralogic.com>
2025-04-12 18:19:58 +0530

    3red commited code

--- index.php
+++ index.php
@@ -94,6 +94,7 @@ $_SESSION["email"] = $uemail;
     </div> 
 
     <h1>this is the wrong message </h1>
+    <h2>this is the 2nd wrong sentence</h2>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
 </body>
